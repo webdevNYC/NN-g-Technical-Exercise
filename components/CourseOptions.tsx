0901a71d1d588e06d23559f8ff0af37f131e7230
Dali Commerce <support@dalicommerce.com>
2024-03-14 12:23:20 -0400

    Add root page course ID and getStaticProps

---
+++ components/CourseOptions.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CourseCardType } from "../interfaces";
+type CourseOptionsProps = {
+  items: CourseCardType[];
+};
+
+const CourseOptions: React.FC<CourseOptionsProps> = ({ items }) => {
+  console.log(items)
+
+  return (
+    <form className='w-[536px] px-8 py-6 bg-white rounded-2xl flex flex-col justify-center items-center gap-6'>
+      <div className='text-black text-4xl font-semibold leading-10'>
+        Course Dates
+      </div>
+      <div>Course Card</div>
+      <button type="submit" className='self-stretch px-4 py-2 bg-red-600 rounded-md justify-center items-center gap-2.5 inline-flex text-center text-white text-2xl font-bold font-["Source Sans Pro"] leading-loose'>
+        Enroll in Course
+      </button>
+    </form>
+  );
+};
+
+export default CourseOptions;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p className="text-xl">
-      <Link href="/about">About</Link>
-    </p>
+  <Layout title="NN/g Senior Frontend Developer - Technical Exercise">
+    <h1 className='text-2xl'>NN/g Senior Frontend Developer</h1>
+    <h3>Technical Exercise</h3>
+    <div className="p-10">
+      <p className="text-xl">
+        Please click on the link below to access the prototype for Course 101. Thank you.
+      </p>
+      <p className='p-10'>
+        <Link href="/course/101">Course 101</Link>
+      </p>
+    </div>
   </Layout>
 );
 
