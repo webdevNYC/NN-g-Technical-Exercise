180f1beb6e24d1cacb84c29e63d279b71605939a
Dali Commerce <support@dalicommerce.com>
2024-03-14 17:49:23 -0400

    Quick Fix for mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,15 +7,17 @@ type Props = {
   title?: string;
 };
 const Layout = ({ children, title = "Course title" }: Props) => (
-  <div>
+  <div className='w-full'>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Header />
-    <div className="px-28"> {children}</div>
-    <footer className="px-28 pt-28">
+    <div className="px-4 sm:px-6 md:px-8 lg:px-16 xl:px-28">
+      {children}
+    </div>
+    <footer className="pt-12 px-4 sm:px-6 md:px-8 lg:px-16 xl:px-28">
       <hr />
       <span className="text-sm text-gray-600">Adriana Fruchter email: adrianafruchter@gmail.com p: 9176120163</span>
     </footer>
