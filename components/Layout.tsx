0bc3dddf7275d39b30f7ec695f60d1ebd0aa2597
Dali Commerce <support@dalicommerce.com>
2024-03-14 11:54:23 -0400

    add Header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,30 +1,23 @@
 import React, { ReactNode } from "react";
-import Link from "next/link";
 import Head from "next/head";
+import Header from "./Header";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
-
-const Layout = ({ children, title = "This is the default title" }: Props) => (
+const Layout = ({ children, title = "Course title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
-        <Link href="/users">Users List</Link> |{" "}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
+    <Header />
+    <div className="px-28"> {children}</div>
+    <footer className="px-28 pt-28">
       <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span className="text-sm text-gray-600">Adriana Fruchter email: adrianafruchter@gmail.com p: 9176120163</span>
     </footer>
   </div>
 );
